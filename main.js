6d51aa0657b3e57d65bbab8a247f1e36366c9e4d
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-15 12:04:49 +0530

    index

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let age=parseInt(prompt("enter your age"));
+
+// if(18<=age ){
+
+// 	if ( confirm("voter id ")) {
+	 
+//  	 console.log( "you go the eligible");
+// 	}
+
+// 	else {
+// 		console.log( "your age is not eligible");
+// 	 }
+// }
+
+
+if(18<=age && confirm("voter id ")){
+
+ 	 console.log( "you go the eligible");
+}
+
+else {
+	console.log( "your age is not eligible");
+ }
+
+
+
+
